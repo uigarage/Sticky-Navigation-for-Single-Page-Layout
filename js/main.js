9e9c03b1832b8fcb18dc9fcb4cd269ab45c4a3fd
Srini Sekhar <sekhar4web@gmail.com>
2013-05-29 15:23:52 +0530

    Re-written the entire logic

--- js/main.js
+++ js/main.js
@@ -1,87 +1,100 @@
-//	d is DOM, contains mostly cached HTML elements.
-//	v is variables, namespacing as much possible
-var d = {}, v = {};
-
-d.primary_menu	= $('#primary_menu');
-d.primary_menu_anc	= d.primary_menu.find('a');
-d.articles	= {};
-d.articles.wrap		= $('#article-wrapper');
-d.articles.items	= $('.article');
-
-d.primary_menu_anc.bind('click', initMenuAction);
-
-v.t	= [];
-
-function initMenuAction(e) {
-	e.preventDefault();
-	var $mi, id, top;
-	$mi	= $(this);
-	id	= $mi.attr('href');
-	//id	= id.substring(1);	// eliminate #
+(function(window, document, undefined){
+	'use strict';
 	
-	top	= parseInt(d.articles.wrap.find(id).position().top);
+	var log	= function(o) {
+		if(window.console)
+			console.log(o);
+	};
 	
+	var stickym	= window.stickym = {
 	
-	$('html, body').animate({scrollTop: top}, 800);
-};
-
-$(window).bind('scroll', function(e) {
-	var $win = $(this);
-	var hilited_menu;
-	var scrollDir = 1;
+		init:	function(options) {
+			return new Stickym(options);
+		},
+		VERSION: '0.1'
+	};
 	
+	var 
+		win			= window,
+		hasProp		= Object.prototype.hasOwnProperty,
+		Math 		= window.Math,
+		getStyle	= window.getComputedStyle,
+		body,
+		documentElement,
+		htmlbody,
+		_instance,
+		elm			= {}
+		;
 	
-	
-	d.articles.items.each(function(idx){
-		var article	= $(this);
-		var top;
-		var speed = 10;
-		var posY;
+	function Stickym(options) {
+		_instance	= this;
 		
+		var 
+			options	= options || {},
+			$w	= $(win)	// window
+		;
 		
-		if(typeof v.t[idx] == 'undefined') {
-			v.t[idx]	= parseInt(article.position().top);
-		}
+		elm.menu				= $('#primary_menu').find('a');				
+		elm.articles			= {};
+		elm.articles.wrap		= $('#article-wrapper');
+		elm.articles.items		= $('.article');
 		
-		var offTop = $win.scrollTop() - v.t[idx];
+		htmlbody	= $('html, body');
 		
-		//	field of vision
-		if(offTop > -($win.height()) && offTop < 0) {
+		$w.bind('load', _instance.init);
+		
+		//	Let the ball roll
+		elm.menu.bind('click', _instance.roll);
+		$w.bind('scroll', _instance.skroll);
+		
+		return _instance;
+	}
+	
+	Stickym.prototype.init	= function() {
+		if( win.location.hash === '' || win.pageYOffset === 0 ) {
+			elm.menu
+				.eq(0)
+				.trigger('click')				
+				.addClass('on');
+		}		
+	};
+	
+	//	scroll page based on menu call to action	
+	Stickym.prototype.roll	= function(e) {
+		e.preventDefault();
+		var 
+			$mi, id, top
+		;
+		$mi	= $(this);
+		id	= $mi.attr('href');
+		//id	= id.substring(1);	// eliminate #
+		
+		top	= parseInt( elm.articles.wrap.find( id ).position().top );
+		htmlbody.animate( {scrollTop: top}, 800 );
+		win.location.hash	= id;
+	};
+
+	Stickym.prototype.skroll	= function(e) {
+		var
+			w	= $(this),
+			wH	= w.height(),
+			scrollTop	= win.pageYOffset
+		;
+
+		elm.articles.items.each( function( inx ){
+			var 
+				article	= $(this),
+				aOffsetTop = scrollTop - parseInt(article.position().top)
+			;			
 			
-			posY	= parseInt(article.css('top'),10);
-			
-			if(posY < 0) {
-				posY = posY * -1;
+			if( aOffsetTop >= -80 && aOffsetTop <= 0 ) {
+				elm.menu
+					.removeClass('on')
+					.filter('[href=#' + article.attr('id')+']')
+					.addClass('on');
 			}
-			
-			posY+=2;
-			
-			console.log('PosY: ' + posY);
-			
-			
-			article.css({
-			//	top: -(posY)   + 'px'
-			});
-			
-			console.log( 'Element ' + article.attr('id') + ' is in the field of vision and top pos is: ' + parseInt(article.css('top'),10));
-			
-		}
-			
-		
-		if( offTop >=-80 && offTop <= 0 ) {		
-			d.primary_menu_anc.removeClass('on');
-			hilited_menu	= d.primary_menu_anc.filter('[href=#' + article.attr('id')+']');
-			hilited_menu.addClass('on');
-		}
-		
-		
-		if( article.attr('id') == 'menu_one') {
-			console.log( article.attr('id') + ' : ' + v.t[idx] + ' Offsetted: ' + offTop  +' Win Scroll Top: ' + $win.scrollTop() + ' Win H: ' + $win.height());
-		}
-		//	Parallax Effect
-		
-	});
-});
+		});
+	};	
+})(window,document);
 
-
-
+var s	= stickym.init({});
